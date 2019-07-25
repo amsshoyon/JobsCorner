86180596bb505142f7f05c1a7e5852468cce375a
amsshoyon <amsshoyon@gmail.com>
2019-07-25 18:40:30 +0600

    Resume download, shortlist

--- resources/views/main/sections/candidates_all.blade.php
+++ resources/views/main/sections/candidates_all.blade.php
@@ -11,9 +11,11 @@
                     <div class="text pl-md-4">
                         <span class="location mb-0"><strong>Job Title:</strong> {{ $Applied->Job->title }}</span>
                         <h2>{{ $Applied->User->firstname }} {{ $Applied->User->lastname }}</h2>
-                        <span class="position">{{ $Applied->User->email }}</span>
+                        <span class="position"><a href="mailto:{{ $Applied->User->email }}" target="_blank">{{ $Applied->User->email }}</a></span>
                         <p>Skills: {{ $Applied->User->skills }}</p>
                         <span class="seen">Applied {{ $Applied->created_at->diffForHumans() }}</span>
+
+                        <p><a href="{{ asset('/resume/'.$Applied->User->Resume->resume) }}">Download Resume</a></p>
                         <p>
 
                             {!!Form::open(['action' => 'JobController@shortlist','method' =>
